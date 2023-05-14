c180d89f29a59eca36ead00403aef725d5e212f1
sasi199 <131953754+sasi199@users.noreply.github.com>
2023-05-14 18:47:37 +0530

    keeper

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-
+import { FaPen } from "react-icons/fa";
 
 function Header() {
   return (
     <header>
       <h1>
      
-        Keeper
-      </h1>
+        Keeper {""} <FaPen />
+       </h1>
     </header>
   );
 }
